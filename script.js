34dc35e0aee4522353ebeb1b9792ceadc9a8de83
lulojrz <lulojrz18@gmail.com>
2024-12-15 21:11:24 -0300

    configuro la llamada a la Api

---
+++ script.js
@@ -0,0 +1,9 @@
+function llamadaApi(ciudad){
+    fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+ ciudad + "?unitGroup=metric&include=days&key=XDDQSTKV57QNT95MTQZQP3EPK&contentType=json")
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+}
+
+
+llamadaApi("");
